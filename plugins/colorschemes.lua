adb2c11b169275c599d47f28467a09ae200ee38b
Leonard Seibold <leo@zrtx.de>
2023-07-29 03:03:25 +0200

    add colorschemes

--- plugins/colorschemes.lua
+++ plugins/colorschemes.lua
@@ -6,4 +6,9 @@ return {
   { "rktjmp/lush.nvim" },
   { "mcchrish/zenbones.nvim" },
   { "projekt0n/github-nvim-theme" },
+  { "antonk52/lake.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "rmehri01/onenord.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  { "NTBBloodbath/doom-one.nvim" },
 }
